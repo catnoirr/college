8a54809cbccb14491e69670a0b4715f07e4f0212
offxsagr <hakies.06@gmail.com>
2024-12-29 10:54:29 +0530

    fixed

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { BackHandler, StyleSheet, View, StatusBar } from 'react-native';
+import { BackHandler, StyleSheet, View, StatusBar, Alert, Button } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
   const webViewRef = useRef(null);
   const [canGoBack, setCanGoBack] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false); // State to handle authentication
 
   useEffect(() => {
+    const checkBiometricAuthentication = async () => {
+      // Check if the device supports biometric authentication
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      if (compatible) {
+        const result = await LocalAuthentication.authenticateAsync({
+          promptMessage: 'Geeta University', // Message shown during authentication
+        });
+
+        if (result.success) {
+          setAuthenticated(true); // Authentication successful
+        } else {
+          Alert.alert('Authentication failed.'); // Authentication failed
+        }
+      } else {
+        Alert.alert('Fingerprint authentication is not supported on this device.');
+      }
+    };
+
+    checkBiometricAuthentication(); // Run authentication check on app load
+
     const handleBackPress = () => {
       if (canGoBack) {
         webViewRef.current.goBack(); // Go back in WebView's history
@@ -21,6 +43,15 @@ export default function App() {
       BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
     };
   }, [canGoBack]);
+
+  if (!authenticated) {
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        <Button title="" onPress={() => setAuthenticated(true)} />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
