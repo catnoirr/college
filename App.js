adb3f3da308baecfbcb415b50d596a77b74091c3
offxsagr <hakies.06@gmail.com>
2024-12-29 10:17:13 +0530

    new app

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { BackHandler, StyleSheet, View, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const webViewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const handleBackPress = () => {
+      if (canGoBack) {
+        webViewRef.current.goBack(); // Go back in WebView's history
+        return true; // Prevent app from closing
+      }
+      return false; // Allow app to close
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+    };
+  }, [canGoBack]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <WebView
+        ref={webViewRef}
+        source={{ uri: 'https://geetauniversity.com' }}
+        style={{ flex: 1 }}
+        onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
+      />
     </View>
   );
 }
@@ -14,7 +39,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
